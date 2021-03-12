0a152a09eebf16a99aba8fe79dc9a00b715aa68b
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 09:31:23 -0600

    Create middleware file for validation and confirm actions endpoint works

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,5 +1,24 @@
 // Write your "actions" router here!
 const express = require('express');
+const Actions = require('./actions-model');
 
-// const router = express.Router();
+const router = express.Router();
 
+// Actions Endpoints
+router.get('/', async (req, res, next) => {
+  try {
+    res.json({msg: "here"})
+  } catch(err) { next(err) }
+});
+
+// Error Handling
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+    custom: "Something went wrong in the actions router"
+  });
+});
+
+// Export
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 // const projectsRouter = require('./projects/projects-router');
-// const actionsRouter = require('./actions/actions-router');
+const actionsRouter = require('./actions/actions-router');
 const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 server.use(express.json());
 // server.use('api/projects', projectsRouter);
-// server.use('api/actions', actionsRouter);
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Use the endpoints for projects and actions in the api</h2>`);
