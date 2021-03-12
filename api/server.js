93a907840c387017743a3394dcba9c75cc6745d9
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 10:05:48 -0600

    Add error catching for general server errors

--- api/server.js
+++ api/server.js
@@ -13,4 +13,12 @@ server.get('/', (req, res) => {
   res.send(`<h2>Use the endpoints for projects and actions in the api</h2>`);
 });
 
+server.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+    custom: "Something went wrong in the server. Please check your endpoint and the structure of your request"
+  });
+});
+
 module.exports = server;
