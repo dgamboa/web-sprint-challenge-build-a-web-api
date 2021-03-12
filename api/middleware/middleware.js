65986554203d7c1f9292ba2da089d6f7eed69df9
Daniel Gamboa <danielgamboa10@gmail.com>
2021-03-12 10:34:59 -0600

    Add validate project and project id middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,4 +1,5 @@
 const Actions = require('../actions/actions-model');
+const Projects = require('../projects/projects-model');
 
 async function validateActionId(req, res, next) {
   const { id } = req.params;
@@ -27,7 +28,34 @@ function validateAction(req, res, next) {
   }
 };
 
+async function validateProjectId(req, res, next) {
+  const { id } = req.params;
+  try {
+    const project = await Projects.get(id);
+    if (project) {
+      req.project = project;
+      next();
+    } else {
+      res.status(404).json({ message: "Project not found" });
+    }
+  } catch(err) { next(err) }
+};
+
+function validateProject(req, res, next) {
+  const { name, description } = req.body;
+
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Missing project data" });
+  } else if (!name || !description) {
+    res.status(400).json({ message: "Missing required field" });
+  } else {
+    next();
+  }
+};
+
 module.exports = {
   validateActionId,
-  validateAction
+  validateAction,
+  validateProjectId,
+  validateProject
 }
